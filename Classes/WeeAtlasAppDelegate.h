49044c38391972d1ecf95b179ced2414799620de
Josh Vickery <vickeryj@setups-MacBook.local>
2010-04-17 14:01:59 -0400

    initial commit of skeleton project

---
+++ Classes/WeeAtlasAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  WeeAtlasAppDelegate.h
+//  WeeAtlas
+//
+//  Created by Josh Vickery on 4/17/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class WeeAtlasViewController;
+
+@interface WeeAtlasAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+    WeeAtlasViewController *viewController;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet WeeAtlasViewController *viewController;
+
+@end
+
---
+++ Classes/WeeAtlasViewController.h
@@ -0,0 +1,16 @@
+//
+//  WeeAtlasViewController.h
+//  WeeAtlas
+//
+//  Created by Josh Vickery on 4/17/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WeeAtlasViewController : UIViewController {
+
+}
+
+@end
+
